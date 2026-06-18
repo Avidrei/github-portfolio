91ca7dc54870a03012bed27da6172052d45bdd51
Avidrei <drcauba@gmail.com>
2026-06-18 20:41:44 +0800

    update  : packages and other pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Poppins, Pixelify_Sans, Montserrat} from "next/font/google";
 import "./globals.css";
 
